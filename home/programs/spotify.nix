a1b387f44ae987fc4efb53f81b8e25a20dec42f0
Atharva <atharva.varpe2019@example.com>
2025-06-10 07:55:29 -0400

    theming spotify

--- home/programs/spotify.nix
+++ home/programs/spotify.nix
@@ -3,20 +3,19 @@
 let
   spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.system};
 in
-{
-  # Spotify with Spicetify customization and ad-blocking
+{  # Spotify with Spicetify customization and transparency
   programs.spicetify = {
     enable = true;
-    
+
     # Basic extensions
     enabledExtensions = with spicePkgs.extensions; [
       adblock
       shuffle
       hidePodcasts
     ];
-    
-    # Catppuccin theme
-    theme = spicePkgs.themes.catppuccin;
-    colorScheme = "mocha";
+
+    # Use Bloom theme - Microsoft Fluent Design inspired
+    theme = spicePkgs.themes.bloom;
+    colorScheme = "comfy";
   };
 }
